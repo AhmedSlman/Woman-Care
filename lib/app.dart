84e85a18c1c3766622439e7f53183e7ea145380a
Ahmed Slman <ahmedslman151@gmail.com>
2024-08-11 17:08:04 +0300

    project setup

---
+++ lib/app.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class WomanCare extends StatelessWidget {
+  const WomanCare({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:woman_care/main.dart';
+import 'package:woman_care/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const WomanCare());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
